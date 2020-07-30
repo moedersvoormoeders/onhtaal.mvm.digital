520ec714644387b4b92c99a0b09bdb7afffdd100
Maartje Eyskens <maartje@eyskens.me>
2020-07-30 20:53:51 +0200

    rm FA module for vue
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import Simplert from 'vue2-simplert-plugin'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-  
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 require('vue2-simplert-plugin/dist/vue2-simplert-plugin.css')
 
 Vue.use(Simplert)
